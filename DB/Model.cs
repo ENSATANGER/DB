2aa3731a5c4a9dbccc44ce47e4f0358027fbe57a
BOULOUQAT-JAWAD <boulouqatjawad22@gmail.com>
2023-03-10 15:21:52 +0100

    mouad's code

--- DB/Model.cs
+++ DB/Model.cs
@@ -134,6 +134,55 @@ namespace DB
             return DictionaryToObject(dico);
         }
 
+        /*public List<dynamic> All()
+        {
+            List<dynamic> L = new List<dynamic>();
+            sql = "select * from" + GetType().Name;
+            IDataReader reader = Connexion.Select(sql);
+
+            while (reader.Read())
+            {
+                Dictionary<string, object> dico = new Dictionary<string, object>();
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                    dico.Add(reader.GetName(i), reader.GetValue(i));
+
+                L.Add(dico);
+            }
+            reader.Close();
+            return L;
+        }*/
+
+        public static List<dynamic> all<T>()
+        {
+            return new List<dynamic>();
+        }
+
+        public List<dynamic> Select(Dictionary<string, object> dico)
+        {
+            List<dynamic> L = new List<dynamic>();
+
+            sql = "select * from " + GetType().Name + " where ";
+            foreach (KeyValuePair<string, object> e in dico)
+                sql += e.Key + "=" + e.Value;
+
+            IDataReader reader = Connexion.Select(sql);
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                    dico.Add(reader.GetName(i), reader.GetValue(i));
+
+                L.Add(dico);
+            }
+            reader.Close();
+            return L;
+        }
+        public static List<dynamic> select<T>(Dictionary<string, object> dico)
+        {
+            return new List<dynamic>();
+        }
+
         //louay's contribution
 
         public static dynamic find<T>(int id)

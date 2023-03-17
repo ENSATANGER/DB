791a34a90ad947a1a512485f865731fa525db253
Mouad Erghout <mouad.erghout.5@gmail.com>
2023-03-17 13:13:53 +0100

    trigger note-moyenne updated

--- DB/tr_insert_note.sql
+++ DB/tr_insert_note.sql
@@ -1,4 +1,4 @@
-CREATE TRIGGER tr_insert_note ON Notes AFTER INSERT AS
+CREATE OR ALTER TRIGGER  tr_insert_note ON Notes AFTER INSERT AS
 BEGIN
     -- Get the student and their level and filiere
     DECLARE @code_eleve varchar(255), @niveau varchar(255), @code_fil varchar(255)
@@ -7,7 +7,8 @@ BEGIN
 
     -- Check if the number of notes matches the number of Matiere records for the student's level and filiere
     DECLARE @num_notes int, @num_matieres int
-    SELECT @num_notes = COUNT(*) FROM Notes WHERE code_eleve = @code_eleve
+    SELECT @num_notes = COUNT(*) FROM Notes WHERE code_eleve = @code_eleve and code_mat IN (
+	 SELECT code FROM Matiere WHERE code_module IN(SELECT code FROM Module WHERE niveau = @niveau AND code_fil = @code_fil))
     SELECT @num_matieres = COUNT(*) FROM Matiere WHERE code_module IN (
         SELECT code FROM Module WHERE niveau = @niveau AND code_fil = @code_fil
     )
